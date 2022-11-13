4ca104e723c735b2cb1a553b8cd6b7ab3f4d260e
karnhao <thehao6294@gmail.com>
2022-11-13 13:33:29 +0700

    update

--- lib/screens/add_product_screen.dart
+++ lib/screens/add_product_screen.dart
@@ -326,10 +326,9 @@ class _AddProdectScreenState extends State<AddProdectScreen> {
     final storageService = Provider.of<StorageService>(context, listen: false);
 
     if (!formKey.currentState!.validate()) {
-      Future.delayed(const Duration(seconds: 2), () {
-        Navigator.pop(context);
-        showSnackBar("Data not founded", backgroundColor: Colors.red);
-      });
+      Navigator.pop(context);
+      showSnackBar("Data not founded", backgroundColor: Colors.red);
+      return;
     }
     formKey.currentState!.save();
 
